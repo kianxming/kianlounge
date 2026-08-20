b5a99c95583e9ee19bbbb9c122eb72591222f938
Kian Kim <kkm19980829@gmail.com>
2026-08-20 10:50:54 +0900

    Add Wano local stronghold economy and garrison data

---
+++ src/v2/data/wano-settlements.js
@@ -0,0 +1,16 @@
+export const WANO_V2_SETTLEMENTS=[
+  {id:'flower_capital',nodeId:'flower_capital',name:'꽃의 수도',ownerFactionId:'kurozumi',money:5200,food:7800,troops:5200,development:72,cap:100,market:74,agriculture:68,morale:86},
+  {id:'ebisu',nodeId:'ebisu',name:'에비스',ownerFactionId:'kurozumi',money:2400,food:3900,troops:2100,development:45,cap:80,market:46,agriculture:40,morale:72},
+  {id:'onigashima',nodeId:'onigashima',name:'오니가시마',ownerFactionId:'beasts',money:4200,food:7200,troops:7000,development:48,cap:70,market:28,agriculture:34,morale:92},
+  {id:'udon_prison',nodeId:'udon_prison',name:'우동 채굴장',ownerFactionId:'beasts',money:2600,food:5200,troops:4300,development:43,cap:75,market:31,agriculture:36,morale:83},
+  {id:'tokage_port',nodeId:'tokage_port',name:'도마뱀항',ownerFactionId:'kozuki',money:1800,food:4100,troops:2600,development:36,cap:70,market:52,agriculture:38,morale:80},
+  {id:'bakura',nodeId:'bakura',name:'바쿠라',ownerFactionId:'beasts',money:3100,food:4800,troops:3600,development:50,cap:80,market:58,agriculture:44,morale:82},
+  {id:'amigasa',nodeId:'amigasa',name:'아미가사',ownerFactionId:'kozuki',money:1400,food:4400,troops:2500,development:31,cap:65,market:35,agriculture:48,morale:84},
+  {id:'itachi_port',nodeId:'itachi_port',name:'이타치항',ownerFactionId:'heart',money:1800,food:3300,troops:2300,development:32,cap:65,market:51,agriculture:34,morale:78},
+  {id:'habu_port',nodeId:'habu_port',name:'하부항',ownerFactionId:'kozuki',money:2000,food:3900,troops:2700,development:38,cap:70,market:55,agriculture:39,morale:80},
+  {id:'mogura_port',nodeId:'mogura_port',name:'모구라항',ownerFactionId:'kid',money:1700,food:3500,troops:2600,development:30,cap:60,market:45,agriculture:32,morale:82},
+  {id:'ringo',nodeId:'ringo',name:'링고',ownerFactionId:'kozuki',money:1300,food:3600,troops:2200,development:30,cap:60,market:32,agriculture:42,morale:86},
+  {id:'kaeru_port',nodeId:'kaeru_port',name:'카에루항',ownerFactionId:'heart',money:1600,food:3700,troops:2100,development:29,cap:60,market:44,agriculture:36,morale:78},
+  {id:'kibi_camp',nodeId:'kibi_camp',name:'키비 주둔지',ownerFactionId:'straw_hat',money:1300,food:4200,troops:2800,development:24,cap:55,market:30,agriculture:44,morale:88},
+  {id:'big_mom_anchorage',nodeId:'big_mom_anchorage',name:'빅 맘 정박지',ownerFactionId:'big_mom',money:2700,food:5200,troops:4200,development:28,cap:55,market:38,agriculture:30,morale:90}
+];
